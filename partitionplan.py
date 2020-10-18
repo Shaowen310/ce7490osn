74771bfecb772fe88a909810b7394b08f543bfb9
Shaowen <shaowen310@outlook.com>
2020-10-18 11:35:58 +0800

    fixed capacity pp

--- partitionplan.py
+++ partitionplan.py
@@ -2,44 +2,47 @@ import numpy as np
 
 
 class PartitionPlan:
-    def __init__(self, n_partitions):
-        self.uid2us = {}
-        self.pid2ps = {i:i for i in range(n_partitions)}
-        # user's storage position to user id
-        self.us2uid = np.empty((0), dtype=np.int64)
-        # partition's storage position to partition id
-        self.ps2pid = np.arange(n_partitions, dtype=np.int64)
+    def __init__(self, n_partitions, user_capacity, partition_capacity):
+        # Assumption: user_id determines the user's storage location
+        # partition_id determines the partition's storage location
+        self.ucap = user_capacity
+        self.pcap = partition_capacity
+        # user storage allocation
+        self.ualloc = np.zeros((self.ucap), dtype=np.bool)
+        # partition storage allocation
+        self.palloc = np.zeros((self.pcap), dtype=np.bool)
+        self.palloc[:n_partitions] = True
         # user primary partition assignment matrix
-        self.user2primary = np.empty((0, n_partitions), dtype=np.bool)
+        self.user2primary = np.zeros((self.ucap, self.pcap), dtype=np.bool)
         # user replica partition assignment matrix
-        self.user2replica = np.empty((0, n_partitions), dtype=np.bool)
-        self.capacity = n_partitions
+        self.user2replica = np.empty((self.ucap, self.pcap), dtype=np.bool)
 
     def partition_least_masters(self):
         master_count = np.sum(self.user2primary, axis=0)
-        master_count[self.ps2pid == -1] = np.nan
+        master_count[~self.palloc] = np.nan
         return self.ps2pid[np.nanargmin(master_count)]
 
     def contains_user(self, user_id):
-        return user_id in self.uid2us
+        return self.ualloc[user_id]
 
     def contains_partition(self, partition_id):
-        return partition_id in self.pid2ps
+        return self.palloc[partition_id]
 
     def _expand_user_capacity(self):
+        pass
 
     def _expand_partition_capacity(self):
+        pass
 
     def partition_add_master(self, partition_id, user_id):
-        us = self.uid2us[user_id]
-        ps = self.pid2ps[partition_id]
-        self.user2primary[us, ps] = True
+        self.ualloc[user_id] = True
+        self.palloc[partition_id] = True
+        self.user2primary[user_id, partition_id] = True
 
     def partition_add_slave(self, partition_id, user_id):
-        us = self.uid2us[user_id]
-        ps = self.pid2ps[partition_id]
-        self.user2replica[us, ps] = True
+        self.ualloc[user_id] = True
+        self.palloc[partition_id] = True
+        self.user2replica[user_id, partition_id] = True
 
     def partition_ids(self):
-
-        return list(range(len(self.partitions)))
+        return np.nonzero(self.palloc)
---
+++ test.py
@@ -0,0 +1,8 @@
+# %%
+from partitionplan import PartitionPlan
+
+# %%
+pp = PartitionPlan(10, 10, 20)
+
+pp.partition_ids()
+# %%
