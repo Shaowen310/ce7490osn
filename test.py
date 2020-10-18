20df7417c98cce23e9af71ca5d758ef9b7e6c681
huayshuo@gmail.com <huayshuo@gmail.com>
2020-10-18 13:57:58 +0800

    up18

--- test.py
+++ test.py
@@ -3,8 +3,11 @@ import numpy as np
 from partitionplan import PartitionPlan
 import spar
 
+action = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 15, 0, 16, 0, 17,
+          0, 18, 0, 19, 0, 20]
+
 # %%
-pp = PartitionPlan(10, 10, 20)
+pp = PartitionPlan(10, 5000, 20)
 
 pp.partition_ids()
 # %%
