53e06eeff2c570575d6b8edd28c7eec651a75f60
Shaowen <shaowen310@outlook.com>
2020-10-18 12:11:52 +0800

    add node done

--- partitionplan.py
+++ partitionplan.py
@@ -15,12 +15,12 @@ class PartitionPlan:
         # user primary partition assignment matrix
         self.user2primary = np.zeros((self.ucap, self.pcap), dtype=np.bool)
         # user replica partition assignment matrix
-        self.user2replica = np.empty((self.ucap, self.pcap), dtype=np.bool)
+        self.user2replica = np.zeros((self.ucap, self.pcap), dtype=np.bool)
 
     def partition_least_masters(self):
-        master_count = np.sum(self.user2primary, axis=0)
-        master_count[~self.palloc] = np.nan
-        return self.ps2pid[np.nanargmin(master_count)]
+        master_count = np.sum(self.user2primary, axis=0, dtype=np.int64)
+        master_count[~self.palloc] = np.iinfo(master_count.dtype).max
+        return np.argmin(master_count)
 
     def contains_user(self, user_id):
         return self.ualloc[user_id]
@@ -39,10 +39,10 @@ class PartitionPlan:
         self.palloc[partition_id] = True
         self.user2primary[user_id, partition_id] = True
 
-    def partition_add_slave(self, partition_id, user_id):
+    def partition_add_slave(self, partition_ids, user_id):
         self.ualloc[user_id] = True
-        self.palloc[partition_id] = True
-        self.user2replica[user_id, partition_id] = True
+        self.palloc[partition_ids] = True
+        self.user2replica[user_id, partition_ids] = True
 
     def partition_ids(self):
-        return np.nonzero(self.palloc)
+        return np.nonzero(self.palloc)[0]
