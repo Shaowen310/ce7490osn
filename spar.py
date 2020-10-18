d2d0dc06e62415cd7b71b485f2fa545ca5642b7c
huayshuo@gmail.com <huayshuo@gmail.com>
2020-10-18 14:03:33 +0800

    up18

--- spar.py
+++ spar.py
@@ -10,3 +10,13 @@ def add_node(pplan, user):
     pids = pplan.partition_ids()
     pids_slave = pids[np.random.choice(len(pids), K)]
     pplan.partition_add_slave(pids_slave, user)
+
+
+def add_edge(pplan, user1, user2):
+    return None
+
+def rm_node(pplan,user):
+    return None
+
+def rm_edge(pplan,user1,user2):
+    return None
