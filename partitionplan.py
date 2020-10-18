de20f7e59356115935955a350e0670118d77a840
Zhou Shaowen <zhoushaowen@ALI-271060N.local>
2020-10-18 16:22:26 +0800

    move master

--- partitionplan.py
+++ partitionplan.py
@@ -38,17 +38,39 @@ class PartitionPlan:
         pass
 
     def partition_add_master(self, partition_id, user_id):
-        self.ualloc[user_id] = True
-        self.palloc[partition_id] = True
+        assert self.palloc[partition_id]
         self.u2p[user_id, partition_id] = self.PRIMARY
+        self.ualloc[user_id] = True
 
     def partition_add_slave(self, partition_ids, user_id):
-        self.ualloc[user_id] = True
-        self.palloc[partition_ids] = True
+        assert np.alltrue(self.palloc[partition_ids])
         self.u2p[user_id, partition_ids] = self.REPLICA
+        self.ualloc[user_id] = True
+
+    def _remove_master(self, user_id):
+        self.u2p[user_id] = np.where(self.u2p == self.PRIMARY, self.NOALLOC, self.u2p)
+
+    def _partition_remove_master(self, partition_id, user_id):
+        self.u2p[user_id, partition_id] = self.NOALLOC
+
+    def partition_remove_master(self, partition_id, user_id):
+        assert self.palloc[partition_id]
+        self._partition_remove_master(partition_id, user_id)
+        self.ualloc[user_id] = not np.alltrue(self.u2p[user_id] == self.NOALLOC)
 
     def partition_ids(self):
         return np.nonzero(self.palloc)[0]
 
     def partition_ids_not_master(self, user_id):
         return np.nonzero(self.palloc & (self.u2p[user_id] != self.PRIMARY))[0]
+
+    def find_partition_having_master(self, user_id):
+        return np.nonzero(self.u2p[user_id] == self.PRIMARY)[0][0]
+
+    def find_partition_having_slave(self, user_id):
+        return np.nonzero(self.u2p[user_id] == self.REPLICA)[0]
+
+    def move_master(self, to_partition_id, user_id):
+        assert self.palloc[to_partition_id]
+        self._remove_master(user_id)
+        self.u2p[user_id, to_partition_id] = self.PRIMARY
