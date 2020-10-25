f18ed70b1c20a1e784f9d0a8665fda66b1acd877
Zhou Shaowen <zhoushaowen@ALI-271060N.local>
2020-10-25 22:46:41 +0800

    edge

--- analyse.py
+++ analyse.py
@@ -6,14 +6,11 @@ import matplotlib.pyplot as plt
 
 
 # %%
-plt.style.use('seaborn-whitegrid')
-
 rx_replica_create = r'(replica create )(\d+)'
 
-
 # %%
-FOLDER = 'facebook_4'
-TEXT = 'facebook4.txt'
+FOLDER = 'facebook_512'
+TEXT = 'facebook256.txt'
 
 palloc = np.load(FOLDER + '/palloc.npy')
 ualloc = np.load(FOLDER + '/ualloc.npy')
@@ -25,11 +22,25 @@ pp.load(FOLDER)
 n_replicas = pp.num_replicas()
 
 print('num_replicas: ', n_replicas)
+print('avg_num_slave_replicas_per_user: ', n_replicas/np.count_nonzero(ualloc)-1)
 
 # %%
 n_masters_per_server = pp.num_masters_per_partition()
-
 print('num_masters_per_server:', n_masters_per_server)
+
+# %%
+# fig = plt.figure()
+# plt.hist(n_masters_per_server)
+
+# %%
+im_n_masters_per_server = np.expand_dims(n_masters_per_server, axis=0)
+
+fig, ax = plt.subplots()
+ax.imshow(im_n_masters_per_server, cmap="plasma", interpolation=None, aspect='auto')
+ax.set_ylim((-1,1))
+# plt.colorbar(orientation="horizontal")
+plt.show()
+
 # %%
 replica_create = []
 
@@ -39,13 +50,39 @@ with open(TEXT) as f:
         replica_create.append(int(re.search(rx_replica_create, line).groups()[1]))
         line = f.readline()
 
+replica_create = np.array(replica_create)
+
+# %%
+plt.style.use('seaborn-whitegrid')
 fig = plt.figure()
 ax = plt.axes()
 ax.set_xlabel('edge creation events')
+ax.set_xlim((0,len(replica_create)))
 ax.set_ylabel('number of replicas created')
+ax.set_ylim((0,np.max(replica_create)))
 
 x = list(range(len(replica_create)))
 
 ax.plot(x, replica_create)
+
+# %%
+hist, bin_edges = np.histogram(replica_create, bins=1000)
+
+cumsum = np.cumsum(hist)
+cumsum = np.hstack(([0], cumsum))
+cumsum = cumsum / cumsum[-1]
+
+print('prob equal or fewer than 2 replica creations: ', np.count_nonzero(replica_create<=2)/len(replica_create))
+
+# %%
+fig = plt.figure()
+ax = plt.axes()
+
+ax.set_xlabel('number of replicas created by event')
+ax.set_xlim((-2,np.max(replica_create)))
+ax.set_ylabel('CDF')
+ax.set_ylim((0,1.05))
+
+ax.plot(bin_edges, cumsum)
 
 # %%
